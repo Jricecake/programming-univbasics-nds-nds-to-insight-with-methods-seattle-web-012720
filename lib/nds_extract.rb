9bdf914399e95a39b01d9a5d870eb43d7e8e90eb
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-17 17:37:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,42 +5,21 @@ require 'pp'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  final_hash = {}
-  directors_index = 0
-  total_gross = 0
-  current_total = 0
-  
-  while directors_index < director_data.length do
-    movies_index = 0
-    while movies_index < director_data[directors_index][:movies].length do
-      current_total += director_data[directors_index][:movies][movies_index][:worldwide_gross]
-      currentname = (director_data[directors_index][:name]).to_s
-      total_gross += director_data[directors_index][:movies][movies_index][:worldwide_gross]
-      movies_index += 1
-      final_hash[currentname] = current_total
-      ##binding.pry
-      end
-    current_total = 0  
-    directors_index += 1
+  director_index = 0
+  director_total = 0
+  directors_grosses = []
+  while director_index < director_data[:movies].length do
+    movie_gross_index = 0
+    while movie_gross_index < director_data[director_index].length
+    director_total += director_data[:movies][director_index][:worldwide_gross]
+    movie_gross_index += 1
+    binding.pry
+    end
+    
   end
-  
-  
-#  director_index = 0
-#  director_total = 0
-#  directors_grosses = []
-#  binding.pry
-#  while director_index < director_data[:movies].length do
-#    movie_gross_index = 0
-#    while movie_gross_index < director_data[director_index].length
-#    director_total += director_data[:movies][director_index][:worldwide_gross]
-#    movie_gross_index += 1
-#
-#    end
-#  directors_grosses[:director_index] = director_total
-#  director_total = 0
-#  director_index += 1
-#  end
-#
+  directors_grosses[:director_index] = director_total
+  director_total = 0
+  director_index += 1
 end
 
 # Write a method that, given an NDS creates a new Hash
